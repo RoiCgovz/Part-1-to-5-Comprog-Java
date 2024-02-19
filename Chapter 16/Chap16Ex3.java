8c5e37a9c59d5bfea9b963dfa9844e6ff6b175e3
Amaw Niya <150252081+AmawNiZy@users.noreply.github.com>
2024-02-19 19:35:16 +0800

    Add files via upload

---
+++ Chapter 16/Chap16Ex3.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Chap16Ex3 
+{
+    public static void main(String[] args) 
+    {
+        int tankCapacity, gasGauge, milesPerGallon;
+        int remainingMiles;
+
+        Scanner scanner = new Scanner(System.in);
+
+     
+        System.out.println("Tank capacity: ");
+        tankCapacity = scanner.nextInt();
+
+        System.out.println("Gage reading: ");
+        gasGauge = scanner.nextInt();
+
+        System.out.print("Miles per gallon: ");
+        milesPerGallon = scanner.nextInt();
+
+        remainingMiles = (int) (tankCapacity * (gasGauge / 100.0)) * milesPerGallon;
+
+        if (remainingMiles >= 200) 
+        {
+            System.out.println("Safe to Proceed");
+        } else 
+        {
+            System.out.println("Get Gas!");
+        }
+        scanner.close();
+    }
+
+
+}
