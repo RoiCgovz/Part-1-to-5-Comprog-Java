39c0609c1ef42e7a7828e462be5f4ed59423262b
Roi <150252081+RoiCgovz@users.noreply.github.com>
2024-05-08 20:02:35 +0800

    Add files via upload

--- Chapter44/WordTriangle.java
+++ Chapter44/WordTriangle.java
@@ -1,18 +1,26 @@
+
 import java.util.Scanner;
-
-import javax.swing.text.AbstractDocument.LeafElement;
-
 public class WordTriangle {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a word --> ");
-        String word = scanner.nextLine();
-        
-        for (int i = 0; i < word.length(); i++) {
-            for (int j = 0; j < i; j++) {
+       Scanner scan = new Scanner(System.in);
+       String word;
+       int wordLength;
+       System.out.print("Enter a word--> ");
+       word = scan.nextLine();
+       
+       if (word.length() % 2 == 1)
+        wordLength = word.length() / 2 + 1;
+       else 
+       wordLength = word.length() / 2;
+
+       for (int i = 0; i < wordLength; i++)
+        {
+            for (int j= 0; j < i; j++)
+            {
                 System.out.print(" ");
             }
-            System.out.println(word.substring(i, word.length() - i));
+            System.out.println(word.substring(i , word.length()- i));
         }
     }
+    
 }
